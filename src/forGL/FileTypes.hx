0e694101f22cd3457b873195bf27360e8628af3a
Randy Maxwell <RMax2015@users.noreply.github.com>
2024-10-25 16:21:45 -0700

    Update FileTypes.hx
    Added support for text files as a type of file. Logs and other stuff.

--- src/forGL/FileTypes.hx
+++ src/forGL/FileTypes.hx
@@ -13,16 +13,18 @@ package forGL;
 
 
 //    List of File types
-@:enum
+enum
 abstract FileTypes(Int) {
 
 	var FILE_IS_UNKNOWN        = 0;
 	
-	var FILE_IS_DICTIONARY     = 1;
+	var FILE_IS_TEXT           = 1;		// .txt or log files like Export log
+
+	var FILE_IS_DICTIONARY     = 2;		// .toml Dictionary
 	
-	var FILE_IS_COMMANDS       = 2;
+//	var FILE_IS_COMMANDS       = 3;		// possible list of commands to forGL app ?
 	
-	var FILE_IS_DATA           = 3;
+//	var FILE_IS_DATA           = 4;		// possible data file to give to forGL app ?
 	
 }
 
