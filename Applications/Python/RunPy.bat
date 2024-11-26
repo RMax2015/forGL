2c7f8906cf72a109ff9dbcabc5495db8cd5b00b2
Randy Maxwell <RMax2015@users.noreply.github.com>
2024-11-26 10:14:11 -0700

    Added remarks to use a Dictionary file as forGL.py arg

--- Applications/Python/RunPy.bat
+++ Applications/Python/RunPy.bat
@@ -1,8 +1,8 @@
 @echo off
+rem     UTF8  Code page on Windows, may not be needed otherwise
 chcp 65001
 
-rem   Run  Python  .py
+rem     Run:  python  forGL.py  dictionary_file
+rem     Please change  dictionary_file  below: to be actual name of your Dictionary file
 
-
-python forGL.py
-
+python  forGL.py  dictionary_file
